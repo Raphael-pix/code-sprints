13d37c5088a37f1555f6c95f32b783c895b7c04c
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2025-04-28 12:43:11 +0300

    test example

---
+++ problems/easy/test_sum_two_numbers.py
@@ -0,0 +1,8 @@
+import pytest
+from sum_two_numbers import sum_two_numbers
+
+def test_sum_two_numbers():
+    assert sum_two_numbers(2, 3) == 5
+    assert sum_two_numbers(-1, 1) == 0
+    assert sum_two_numbers(0, 0) == 0
+    assert sum_two_numbers(123, 456) == 579
