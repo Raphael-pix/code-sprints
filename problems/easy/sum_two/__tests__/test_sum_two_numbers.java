28a39e0697a6d019dd3826c68d590721879f19a4
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2025-04-29 15:52:15 +0300

    renaming files

--- problems/easy/sum_two/__tests__/test_sum_two_numbers.java
+++ problems/easy/sum_two/__tests__/test_sum_two_numbers.java
@@ -1,5 +1,5 @@
 package problems.easy.sum_two.__tests__;
 
-public class _test_sum_two {
+public class test_sum_two_numbers {
     
 }
--- problems/easy/sum_two/__tests__/test_sum_two_numbers.js
+++ problems/easy/sum_two/__tests__/test_sum_two_numbers.js
@@ -1,4 +1,4 @@
-const sum_two = require('../sum_two');
+const sum_two = require('../sum_two_numbers');
 
 test('adds 2 + 3 to equal 5', () => {
   expect(sum_two(2, 3)).toBe(5);
