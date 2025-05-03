b872a71e0bc27ee92686b0cd3f1ce286ef7e8609
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-03 19:24:54 +0300

    adding new challenge-remove elements

---
+++ problems/intermediate/remove_element/__tests__/test_remove_elements.py
@@ -0,0 +1,17 @@
+from remove_element import removeElement
+
+# Test case 1
+nums = [3, 2, 2, 3]
+val = 3
+k = removeElement(nums, val)
+assert k == 2
+assert nums[:k] == [2, 2]
+
+# Test case 2
+nums = [0, 1, 2, 2, 3, 0, 4, 2]
+val = 2
+k = removeElement(nums, val)
+assert k == 5
+assert nums[:k] == [0, 1, 4, 0, 3]
+
+print("All tests passed!")
