981eea026d50c661dab1cd1fd2467169d279066b
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-03 14:08:07 +0300

    updating YML file and c/c++ code

--- problems/easy/sum_two/sum_two_numbers.java
+++ problems/easy/sum_two/sum_two_numbers.java
@@ -3,6 +3,7 @@ package problems.easy.sum_two;
 public class sum_two_numbers {
 
     public static int sum_two(int a, int b) {
-        return a + b;
+        // write your code here
+        return 0;
     }
 }
