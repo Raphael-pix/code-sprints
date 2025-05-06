93733d8b0eddeaf2da9fa22085233d3455660639
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-06 21:10:27 +0300

    Update grading_students.py

--- problems/easy/grading_students/grading_students.py
+++ problems/easy/grading_students/grading_students.py
@@ -1,4 +1,13 @@
 
 def gradingStudents(grades):
-    # Write your code here
-    return grades
+    result = []
+    for grade in grades:
+        if grade < 38:
+            result.append(grade)
+        else:
+            next_multiple_of_5 = ((grade // 5) + 1) * 5
+            if next_multiple_of_5 - grade < 3:
+                result.append(next_multiple_of_5)
+            else:
+                result.append(grade)
+    return result
