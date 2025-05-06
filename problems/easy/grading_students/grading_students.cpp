fdd1a578497062ca73604b72805f4da8d135f912
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-06 21:40:21 +0300

    Update grading_students.cpp

--- problems/easy/grading_students/grading_students.cpp
+++ problems/easy/grading_students/grading_students.cpp
@@ -8,5 +8,18 @@ using namespace std;
  */
 
 vector<int> gradingStudents(vector<int> grades) {
-    return grades;
+   vector<int> result;
+    for (int grade : grades) {
+        if (grade < 38) {
+            result.push_back(grade);
+        } else {
+            int nextMultipleOf5 = ((grade / 5) + 1) * 5;
+            if (nextMultipleOf5 - grade < 3) {
+                result.push_back(nextMultipleOf5);
+            } else {
+                result.push_back(grade);
+            }
+        }
+    }
+    return result;
 }
