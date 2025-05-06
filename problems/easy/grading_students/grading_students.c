0a91afc008a0286d4f1613e7a0bb2a75071a6786
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-06 21:12:56 +0300

    Adding comments to grading students challenge

--- problems/easy/grading_students/grading_students.c
+++ problems/easy/grading_students/grading_students.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Rounds the grades according to the rules.
+ * @param grades array of student grades
+ * @param n size of array
+ * @param result output array to store rounded grades
+ */
+
 int* gradingStudents(int grades[], int grades_count) {
     int* result = (int*)malloc(sizeof(int) * grades_count);
     for (int i = 0; i < grades_count; i++) {
