124513a2bdefd4c97d80326edf395c12423e9c9e
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 19:42:14 +0300

    Update programmer_day.cpp

--- problems/easy/programmer_day/programmer_day.cpp
+++ problems/easy/programmer_day/programmer_day.cpp
@@ -8,6 +8,12 @@ using namespace std;
  */
 
 string dayOfProgrammer(int year) {
-    // Write your code here
-    return "12.09." + to_string(year);
+     if (year == 1918) {
+        return "26.09." + to_string(year);
+    } else if ((year >= 1700 && year <= 1917 && year % 4 == 0) ||
+               (year >= 1919 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))) {
+        return "12.09." + to_string(year);
+    } else {
+        return "13.09." + to_string(year);
+    }
 }
