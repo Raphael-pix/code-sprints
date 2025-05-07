4b07f0d8239a3e908575436f3e7391b5d9252839
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 22:40:39 +0300

    Update OrganizingContainer.java

--- problems/intermediate/organizing_container/OrganizingContainer.java
+++ problems/intermediate/organizing_container/OrganizingContainer.java
@@ -10,7 +10,21 @@ public class OrganizingContainer {
      * @return 'Possible' if reorganization is possible, otherwise 'Impossible'
      */
     public static String organizingContainers(List<List<Integer>> container) {
-        // Write your code here
-        return "Impossible";
+        int n = container.size();
+        List<Long> rowSum = new ArrayList<>(Collections.nCopies(n, 0L));
+        List<Long> colSum = new ArrayList<>(Collections.nCopies(n, 0L));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int val = container.get(i).get(j);
+                rowSum.set(i, rowSum.get(i) + val);
+                colSum.set(j, colSum.get(j) + val);
+            }
+        }
+
+        Collections.sort(rowSum);
+        Collections.sort(colSum);
+
+        return rowSum.equals(colSum) ? "Possible" : "Impossible";
     }
 }
