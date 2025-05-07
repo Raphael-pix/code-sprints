580628576e67fe419afbfb198dafd669d90bd560
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 22:49:20 +0300

    Update morgan_string.py

--- problems/hard/morgan_and_string/morgan_string.py
+++ problems/hard/morgan_and_string/morgan_string.py
@@ -1,3 +1,13 @@
 def morganAndString(a, b):
-    # Write your code here
-    pass
+    result = []
+    i, j = 0, 0
+    while i < len(a) and j < len(b):
+        if a[i:] <= b[j:]:
+            result.append(a[i])
+            i += 1
+        else:
+            result.append(b[j])
+            j += 1
+    result.append(a[i:])
+    result.append(b[j:])
+    return ''.join(result)
