dd5a5b81235b0760ade98f538c74a0aae9bedb88
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 20:05:27 +0300

    Update climbing_leaderboard.cpp

--- problems/intermediate/climbing_leaderboard/climbing_leaderboard.cpp
+++ problems/intermediate/climbing_leaderboard/climbing_leaderboard.cpp
@@ -9,7 +9,22 @@ using namespace std;
 */
 
 vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
-    vector<int> result;
-    // Write your code here
-    return result;
+    vector<int> unique_ranked;
+    unique_ranked.push_back(ranked[0]);
+    for (int i = 1; i < ranked.size(); ++i) {
+        if (ranked[i] != ranked[i - 1]) {
+            unique_ranked.push_back(ranked[i]);
+        }
+    }
+
+    vector<int> res;
+    int i = unique_ranked.size() - 1;
+
+    for (int score : player) {
+        while (i >= 0 && score >= unique_ranked[i]) {
+            i--;
+        }
+        res.push_back(i + 2);  // +2 for 1-based and extra back
+    }
+    return res;
 }
