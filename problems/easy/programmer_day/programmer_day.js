2922e42ad020b077c89fa062b589ef7e47abb6a3
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 19:39:01 +0300

    Update programmer_day.js

--- problems/easy/programmer_day/programmer_day.js
+++ problems/easy/programmer_day/programmer_day.js
@@ -5,8 +5,14 @@
  */
 
 function dayOfProgrammer(year) {
-    // Write your code here
-    return `12.09.${year}`;
+     if (year === 1918) {
+        return `26.09.${year}`;
+    } else if ((year >= 1700 && year <= 1917 && year % 4 === 0) ||
+               (year >= 1919 && (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)))) {
+        return `12.09.${year}`;
+    } else {
+        return `13.09.${year}`;
+    }
 }
 
 module.exports = { dayOfProgrammer };
