37ac4d944800305ae0be4abb5beab036e9d26cac
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 20:09:17 +0300

    Update ClimbingLeaderboard.java

--- problems/intermediate/climbing_leaderboard/ClimbingLeaderboard.java
+++ problems/intermediate/climbing_leaderboard/ClimbingLeaderboard.java
@@ -11,8 +11,25 @@ public class ClimbingLeaderboard {
      */
     
     public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+        List<Integer> uniqueRanked = new ArrayList<>();
+        uniqueRanked.add(ranked.get(0));
+
+        // Remove duplicates
+        for (int i = 1; i < ranked.size(); i++) {
+            if (!ranked.get(i).equals(ranked.get(i - 1))) {
+                uniqueRanked.add(ranked.get(i));
+            }
+        }
+
         List<Integer> result = new ArrayList<>();
-        // Write your code here
+        int i = uniqueRanked.size() - 1;
+
+        for (int score : player) {
+            while (i >= 0 && score >= uniqueRanked.get(i)) {
+                i--;
+            }
+            result.add(i + 2);  // +2 because of 1-based index and extra move back
+        }
         return result;
     }
 }
