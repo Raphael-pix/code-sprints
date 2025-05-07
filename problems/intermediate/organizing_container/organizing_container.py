4d4c91b8ba59e13545b39c4b3aea2dbbec27f80b
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 22:26:22 +0300

    Update organizing_container.py

--- problems/intermediate/organizing_container/organizing_container.py
+++ problems/intermediate/organizing_container/organizing_container.py
@@ -1,4 +1,6 @@
 
 def organizingContainers(container):
-    # Write your code here
-    return "Impossible"
+    n = len(container)
+    row_sums = [sum(row) for row in container]
+    col_sums = [sum(container[i][j] for i in range(n)) for j in range(n)]
+    return "Possible" if sorted(row_sums) == sorted(col_sums) else "Impossible"
