1387bf04c0d4edcca7ccefca96e65cc3213af9f9
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 14:10:16 +0300

    Adding parameter explanatory comments

--- problems/easy/angry_professor/angry_professor.cpp
+++ problems/easy/angry_professor/angry_professor.cpp
@@ -2,6 +2,13 @@
 #include <string>
 using namespace std;
 
+/**
+ * Determines if the class is cancelled.
+ * @param k threshold number of students
+ * @param a arrival times
+ * @return "YES" if class is cancelled, "NO" otherwise
+ */
+
 string angryProfessor(int k, vector<int> a) {
     // Write your code here
     return "YES";
--- problems/easy/angry_professor/angry_professor.js
+++ problems/easy/angry_professor/angry_professor.js
@@ -1,3 +1,9 @@
+/**
+ * Determines if the class is cancelled.
+ * @param {number} k - threshold number of students
+ * @param {number[]} a - arrival times
+ * @returns {string} - "YES" if class is cancelled, "NO" otherwise
+ */
 
 function angryProfessor(k, a) {
     // Write your code here
