d20d2ab502deebdaa84eb02f8255d9eb22675124
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 19:52:54 +0300

    updating explanatory comments

--- problems/intermediate/climbing_leaderboard/climbing_leaderboard.js
+++ problems/intermediate/climbing_leaderboard/climbing_leaderboard.js
@@ -1,3 +1,9 @@
+/**
+ * Returns the player's rank after each game.
+ * @param {number[]} ranked
+ * @param {number[]} player
+ * @returns {number[]}
+ */
 
 function climbingLeaderboard(ranked, player) {
     // Write your code here
