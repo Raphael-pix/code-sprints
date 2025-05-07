1af667b0f823c0a1e2e1ceaabb3be91dcbb00d82
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 22:33:09 +0300

    Update organizing_container.cpp

--- problems/intermediate/organizing_container/organizing_container.cpp
+++ problems/intermediate/organizing_container/organizing_container.cpp
@@ -10,6 +10,17 @@ using namespace std;
  */
 
 string organizingContainers(vector<vector<int>> container) {
-    // Write your code here
-    return "Impossible";
+     int n = container.size();
+    vector<long long> rowSum(n, 0), colSum(n, 0);
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j) {
+            rowSum[i] += container[i][j];
+            colSum[j] += container[i][j];
+        }
+
+    sort(rowSum.begin(), rowSum.end());
+    sort(colSum.begin(), colSum.end());
+
+    return rowSum == colSum ? "Possible" : "Impossible";
 }
