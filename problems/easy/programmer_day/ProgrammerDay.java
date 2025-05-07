45fa8384c8ba5ebf78e06148e39bf366913ac874
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 19:44:17 +0300

    Update ProgrammerDay.java

--- problems/easy/programmer_day/ProgrammerDay.java
+++ problems/easy/programmer_day/ProgrammerDay.java
@@ -8,7 +8,13 @@ public class ProgrammerDay {
      * @returns {string} in format dd.mm.yyyy
      */
     public static String dayOfProgrammer(int year) {
-        // Write your code here
-        return String.format("12.09.%d", year);
+         if (year == 1918) {
+            return String.format("26.09.%d", year);
+        } else if ((year >= 1700 && year <= 1917 && year % 4 == 0) ||
+                   (year >= 1919 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))) {
+            return String.format("12.09.%d", year);
+        } else {
+            return String.format("13.09.%d", year);
+        }
     }
 }
