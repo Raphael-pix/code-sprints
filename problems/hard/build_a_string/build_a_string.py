c457033e9e0b49f5e1af7218dfcd011a534955e7
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 23:32:52 +0300

    Update build_a_string.py

--- problems/hard/build_a_string/build_a_string.py
+++ problems/hard/build_a_string/build_a_string.py
@@ -1,4 +1,17 @@
 
 def buildString(a, b, s):
-    # Write your code here
-    return 0  
+    n = len(s)
+    dp = [0] * (n + 1)  # dp[i]: min cost to build s[0:i]
+    
+    for i in range(1, n + 1):
+        # Option 1: add next character
+        dp[i] = dp[i - 1] + a
+
+        # Option 2: try to find a substring to copy
+        for j in range(i // 2):
+            length = i - j
+            if s[j:j+length] == s[i-length:i]:
+                dp[i] = min(dp[i], dp[i-length] + b)
+                break  # take the first match (longest possible)
+
+    return dp[n]
