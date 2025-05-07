f7318ce1fc596a02c9fe2c98816011f6976f96a1
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 22:08:50 +0300

    Update ema_supercomputer.cpp

--- problems/intermediate/ema_supercomputer/ema_supercomputer.cpp
+++ problems/intermediate/ema_supercomputer/ema_supercomputer.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <set>
 using namespace std;
 
 /**
@@ -10,5 +11,49 @@ using namespace std;
  */
 
 int twoPluses(vector<string> grid) {
-    return 0;
+     int n = grid.size(), m = grid[0].size();
+    vector<set<pair<int, int>>> pluses;
+
+    auto maxArm = [&](int i, int j) {
+        int length = 0;
+        while (
+            i - length >= 0 && i + length < n &&
+            j - length >= 0 && j + length < m &&
+            grid[i - length][j] == 'G' && grid[i + length][j] == 'G' &&
+            grid[i][j - length] == 'G' && grid[i][j + length] == 'G'
+        ) {
+            set<pair<int, int>> cells;
+            for (int d = -length; d <= length; d++) {
+                cells.insert({i + d, j});
+                cells.insert({i, j + d});
+            }
+            pluses.push_back(cells);
+            length++;
+        }
+    };
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (grid[i][j] == 'G') {
+                maxArm(i, j);
+            }
+        }
+    }
+
+    int maxProduct = 0;
+    for (int i = 0; i < pluses.size(); i++) {
+        for (int j = i + 1; j < pluses.size(); j++) {
+            bool overlap = false;
+            for (auto &cell : pluses[i]) {
+                if (pluses[j].count(cell)) {
+                    overlap = true;
+                    break;
+                }
+            }
+            if (!overlap) {
+                maxProduct = max(maxProduct, (int)pluses[i].size() * (int)pluses[j].size());
+            }
+        }
+    }
+    return maxProduct;
 }
