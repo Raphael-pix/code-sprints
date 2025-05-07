8cb63d38203c75790b1bdd7d7cd3f9721e32b781
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 19:36:27 +0300

    Adding explanatory comments

--- problems/easy/programmer_day/programmer_day.c
+++ problems/easy/programmer_day/programmer_day.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* dayOfProgrammer(int year) {
+/**
+ * Returns the 256th day of the year in Russia for the given year.
+ * @param {number} year
+ * @returns {string} in format dd.mm.yyyy
+ */
+
+char *dayOfProgrammer(int year){
     // Write your code here
-    char* result = (char*)malloc(20);
+    char *result = (char *)malloc(20);
     sprintf(result, "12.09.%d", year);
     return result;
 }
--- problems/easy/programmer_day/programmer_day.js
+++ problems/easy/programmer_day/programmer_day.js
@@ -1,3 +1,8 @@
+/**
+ * Returns the 256th day of the year in Russia for the given year.
+ * @param {number} year
+ * @returns {string} in format dd.mm.yyyy
+ */
 
 function dayOfProgrammer(year) {
     // Write your code here
