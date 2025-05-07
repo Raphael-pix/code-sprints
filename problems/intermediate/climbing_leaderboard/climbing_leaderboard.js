bfd95f90b944e6ab64bb09a62bc6db54b93d5676
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-07 19:56:30 +0300

    Update climbing_leaderboard.js

--- problems/intermediate/climbing_leaderboard/climbing_leaderboard.js
+++ problems/intermediate/climbing_leaderboard/climbing_leaderboard.js
@@ -6,8 +6,17 @@
  */
 
 function climbingLeaderboard(ranked, player) {
-    // Write your code here
-    return [];
+    let uniqueRanked = [...new Set(ranked)];
+    let result = [];
+    let i = uniqueRanked.length - 1;
+
+    for (let score of player) {
+        while (i >= 0 && score >= uniqueRanked[i]) {
+            i--;
+        }
+        result.push(i + 2);  // +2 for 1-based index and moving one extra back
+    }
+    return result;
 }
 
 module.exports = { climbingLeaderboard };
