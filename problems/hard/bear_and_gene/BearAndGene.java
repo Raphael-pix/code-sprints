7af76689b94746d52a4aed4907786ba83c0a5a0c
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-08 22:24:37 +0300

    Update BearAndGene.java

--- problems/hard/bear_and_gene/BearAndGene.java
+++ problems/hard/bear_and_gene/BearAndGene.java
@@ -4,9 +4,29 @@ import java.util.Scanner;
 public class BearAndGene {
 
     public static int steadyGene(String gene) {
-        // Write your code goes here
+         int n = gene.length();
+        int required = n / 4;
+        int[] count = new int[128]; // For A, C, G, T
+        for (char c : gene.toCharArray()) {
+            count[c]++;
+        }
 
-        return 0;
+        if (count['A'] == required && count['C'] == required &&
+            count['G'] == required && count['T'] == required) {
+            return 0;
+        }
+
+        int minLen = n, left = 0;
+        for (int right = 0; right < n; right++) {
+            count[gene.charAt(right)]--;
+            while (left < n && count['A'] <= required && count['C'] <= required &&
+                   count['G'] <= required && count['T'] <= required) {
+                minLen = Math.min(minLen, right - left + 1);
+                count[gene.charAt(left)]++;
+                left++;
+            }
+        }
+        return minLen;
     }
 
     public static void main(String[] args) {
