1d53fbf9246b4e214a3a4dac9c0d34bc54739431
Raphael Kaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-08 22:31:37 +0300

    Update bear_and_gene.cpp

--- problems/hard/bear_and_gene/bear_and_gene.cpp
+++ problems/hard/bear_and_gene/bear_and_gene.cpp
@@ -3,8 +3,26 @@
 using namespace std;
 
 int steadyGene(string gene) {
-    // Write your code goes here
+     int n = gene.size();
+    int required = n / 4;
+    int count[128] = {0};
 
-    return 0;
+    for (char c : gene) count[c]++;
+
+    if (count['A'] == required && count['C'] == required &&
+        count['G'] == required && count['T'] == required)
+        return 0;
+
+    int minLen = n, left = 0;
+    for (int right = 0; right < n; right++) {
+        count[gene[right]]--;
+        while (left < n && count['A'] <= required && count['C'] <= required &&
+               count['G'] <= required && count['T'] <= required) {
+            minLen = min(minLen, right - left + 1);
+            count[gene[left]]++;
+            left++;
+        }
+    }
+    return minLen;
 }
 
